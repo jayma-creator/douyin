2d080b499bc05a084117e8e78f58dfb215e0b745
jayma-creator <476525881@qq.com>
2022-10-04 20:37:09 +0800

    优化表结构

--- common/common.go
+++ common/common.go
@@ -15,13 +15,13 @@ type Video struct {
 	Id            int64          `json:"id,omitempty"`
 	AuthorId      int64          `json:"author_id"`
 	Author        User           `json:"author" gorm:"foreignKey:AuthorId"`
-	PlayUrl       string         `json:"play_url,omitempty"`
-	CoverUrl      string         `json:"cover_url,omitempty"`
-	FavoriteCount int64          `json:"favorite_count,omitempty"`
-	CommentCount  int64          `json:"comment_count,omitempty"`
-	IsFavorite    bool           `json:"is_favorite,omitempty"`
-	Title         string         `json:"title"`
-	CreatedAt     time.Time      `json:"created_at"`
+	PlayUrl       string         `json:"play_url,omitempty" gorm:"NOT NULL"`
+	CoverUrl      string         `json:"cover_url,omitempty" gorm:"NOT NULL"`
+	FavoriteCount int64          `json:"favorite_count,omitempty" gorm:"NOT NULL"`
+	CommentCount  int64          `json:"comment_count,omitempty" gorm:"NOT NULL"`
+	IsFavorite    bool           `json:"is_favorite,omitempty" gorm:"NOT NULL"`
+	Title         string         `json:"title" gorm:"NOT NULL"`
+	CreatedAt     time.Time      `json:"created_at" gorm:"index"`
 	UpdatedAt     time.Time      `json:"updated_at"`
 	DeletedAt     gorm.DeletedAt `json:"deleted_at"`
 }
@@ -32,21 +32,21 @@ type Comment struct {
 	User       User           `json:"user" gorm:"foreignKey:UserId"`
 	VideoId    int64          `json:"video_id" gorm:"index:idx"`
 	Video      Video          `json:"video" gorm:"foreignKey:VideoId"`
-	Content    string         `json:"content,omitempty"`
-	CreateDate string         `json:"create_date,omitempty"`
-	CreatedAt  time.Time      `json:"created_at"`
+	Content    string         `json:"content,omitempty" gorm:"NOT NULL"`
+	CreateDate string         `json:"create_date,omitempty" gorm:"NOT NULL"`
+	CreatedAt  time.Time      `json:"created_at" gorm:"index"`
 	UpdatedAt  time.Time      `json:"updated_at"`
 	DeletedAt  gorm.DeletedAt `json:"deleted_at"`
 }
 
 type User struct {
 	Id            int64          `json:"id,omitempty"`
-	Name          string         `json:"name,omitempty" gorm:"index:idx;unique"`
-	FollowCount   int64          `json:"follow_count,omitempty"`
-	FollowerCount int64          `json:"follower_count,omitempty"`
-	IsFollow      bool           `json:"is_follow,omitempty" `
-	Password      string         `json:"password,omitempty"`
-	CreatedAt     time.Time      `json:"created_at"`
+	Name          string         `json:"name,omitempty" gorm:"index:idx;unique;NOT NUll"`
+	FollowCount   int64          `json:"follow_count,omitempty" gorm:"NOT NULL"`
+	FollowerCount int64          `json:"follower_count,omitempty" gorm:"NOT NULL"`
+	IsFollow      bool           `json:"is_follow,omitempty" gorm:"NOT NULL"`
+	Password      string         `json:"password,omitempty" gorm:"NOT NULL"`
+	CreatedAt     time.Time      `json:"created_at" gorm:"index"`
 	UpdatedAt     time.Time      `json:"updated_at"`
 	DeletedAt     gorm.DeletedAt `json:"deleted_at"`
 }
